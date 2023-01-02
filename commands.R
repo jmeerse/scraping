0a5245dd7310610043150b594167db3ae815954c
jmeerse <jmeerse@gmail.com>
2023-01-02 10:06:41 -0500

    update 1/2/23

--- commands.R
+++ commands.R
@@ -29,6 +29,81 @@ prem <- full_table %>%
 # make row 1 column names
 #prem <- prem %>% row_to_names(1)
 
+prem <- prem %>% select(-`Last 5`) #so that the rest of the data lines up
+
+urls <- c("https://fbref.com/en/comps/9/2021-2022/2021-2022-Premier-League-Stats",
+          "https://fbref.com/en/comps/9/2020-2021/2020-2021-Premier-League-Stats",
+          "https://fbref.com/en/comps/9/2019-2020/2019-2020-Premier-League-Stats",
+          "https://fbref.com/en/comps/9/2018-2019/2018-2019-Premier-League-Stats"
+          )
+for (i in urls) {
+full_table <- read_html(i, as.data.frame=T, stringsAsFactors = TRUE)
+# html_nodes to pull all nodes under the "table" label
+# the number (in this case 2) tells which table to pull from the list of tables
+# html_table converts it to table format
+prem1 <- full_table %>%  
+  html_nodes("table") %>% 
+  .[[1]] %>% 
+  html_table(fill=T) 
+
+prem <- rbind(prem, prem1)
+}
+
+nums <- seq(1:length(prem$GF))
+prem$nums <- nums
+prem$cuts <- cut(prem$nums, 5, labels = F)
+prem <- prem %>% mutate(season = case_when(cuts == 1 ~ "2023",
+                                  cuts == 2 ~ "2022",
+                                  cuts == 3 ~ "2021",
+                                  cuts == 4 ~ "2020",
+                                  cuts == 5 ~ "2019")
+)
+
+prem %>% ggplot(aes(x = GF, y = Pts)) + geom_point()
+cor(x = prem$GF, y = prem$Pts)
+
+prem <- prem %>% mutate(pct = (W + 0.5*D)/MP)
+prem <- prem %>% mutate(pyth_pct = GF^1.2 / (GF^1.2 + GA ^ 1.2))
+
+cor(x = prem$pct, y = prem$pyth_pct)
+
+#looking for best exponent
+exp <- seq(1, 20, by = 0.1)
+best_exp <- data.frame(rmse = numeric(0))
+dat <- data.frame(rmse = numeric(1))
+
+for (i in exp) {
+  dat$rmse <- sqrt(mean(
+    (prem$pct - ((prem$GF ^ i)/((prem$GF ^ i) + (prem$GA ^ i))))^2))
+  
+  best_exp <- rbind(best_exp, dat)
+}
+
+best_exp$exp <- exp
+
+best_exp %>% ggplot(aes(x = exp, y = rmse)) + geom_point()
+
+min(best_exp$rmse) #so with exp = 1.2, rmse is minimized
+
+#what's more important, pct or pts?
+prem %>% filter(season < 2023) %>% ggplot(aes(x = pyth_pct, y = Pts)) + geom_point()
+
+
+
+
+
+
+prem2 <- prem %>% filter(season < 2023)
+cor(x = prem2$GD, y = prem2$Pts)
+cor(x = prem2$GF, y = prem2$Pts)
+cor(x = prem2$Pts, y = prem2$pct)
+cor(x = prem$Pts, y = prem$pyth_pct)
+
+prem2 %>% ggplot(aes(x = GD, y = Pts)) +
+  geom_point() +
+  geom_text_repel(data = filter(prem2, GD > 40), aes(x = GD, y = Pts, label = Squad)) +
+  geom_text(data = filter(prem2, GD > 40), aes(x = GD, y = Pts, label = season), nudge_y = -3)
+  
 
 prem <- left_join(prem, EPL_logos, by = "Squad")
 
